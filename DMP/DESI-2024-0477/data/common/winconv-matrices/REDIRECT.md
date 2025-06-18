b0e5d7241778e7b27cd418e89d9013455bd233f5
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2025-06-19 00:20:32 +0100

    Upload Zenodo archive for DESI-2024-0477 DMP
    Large files are redirected to the Zenodo record

---
+++ DMP/DESI-2024-0477/data/common/winconv-matrices/REDIRECT.md
@@ -0,0 +1,1 @@
+Please see the equivalent contents in: https://zenodo.org/records/15611866/preview/DESI-2024-0477.zip?include_deleted=0#tree_item21
---
+++ DMP/DESI-2024-0477/figs/config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+
+ROOTDIR = Path(__file__).parent.parent
+DATADIR = ROOTDIR / "data"
